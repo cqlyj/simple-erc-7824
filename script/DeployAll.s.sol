8e0bc65d957116ea3169e0044e4dba371adbc28b
cqlyj <luoyingjie0721@gmail.com>
2025-05-21 19:59:02 +0800

    feat: update DeployAll script to use Adjudicator and Custody contracts

--- script/DeployAll.s.sol
+++ script/DeployAll.s.sol
@@ -3,9 +3,8 @@ pragma solidity 0.8.28;
 
 import {Script, console} from "forge-std/Script.sol";
 import {MockUSDC} from "src/MockUSDC.sol";
-import {NitroAdjudicator} from "src/nitro/NitroAdjudicator.sol";
-import {MicroPaymentApp} from "src/nitro/MicroPaymentApp.sol";
-import {MultiAssetHolder} from "src/nitro/MultiAssetHolder.sol";
+import {Adjudicator} from "src/nitrolite/Adjudicator.sol";
+import {Custody} from "src/nitrolite/Custody.sol";
 
 contract DeployAll is Script {
     function run() external {
@@ -14,17 +13,11 @@ contract DeployAll is Script {
         MockUSDC usdc = new MockUSDC();
         console.log("MockUSDC deployed at: ", address(usdc));
 
-        NitroAdjudicator adjudicator = new NitroAdjudicator();
-        console.log("NitroAdjudicator deployed at: ", address(adjudicator));
+        Adjudicator adjudicator = new Adjudicator();
+        console.log("Adjudicator deployed at: ", address(adjudicator));
 
-        MicroPaymentApp app = new MicroPaymentApp();
-        console.log("MicroPaymentApp deployed at: ", address(app));
-
-        MultiAssetHolder multiAssetHolder = new MultiAssetHolder();
-        console.log(
-            "MultiAssetHolder deployed at: ",
-            address(multiAssetHolder)
-        );
+        Custody custody = new Custody();
+        console.log("Custody deployed at: ", address(custody));
 
         vm.stopBroadcast();
     }
